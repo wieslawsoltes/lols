37f91dc9d959145d3ea6d759c4f27c1c15237776
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2022-10-30 00:57:21 +0200

    Update MainWindow.axaml.cs

--- lols/MainWindow.axaml.cs
+++ lols/MainWindow.axaml.cs
@@ -3,12 +3,8 @@ using System.Diagnostics;
 using System.Globalization;
 using System.Threading;
 using System.Threading.Tasks;
-using Avalonia;
 using Avalonia.Controls;
-using Avalonia.Media;
-using Avalonia.Media.Immutable;
 using Avalonia.Threading;
-using Metsys.Bson;
 
 namespace lols;
 
@@ -27,8 +23,7 @@ public partial class MainWindow : Window
     protected override void OnOpened(EventArgs e)
     {
         base.OnOpened(e);
-        
-        
+
         timer.Elapsed += OnTimer;
 
         stopwatch.Start();
@@ -48,39 +43,9 @@ public partial class MainWindow : Window
     void RunTest()
     {
         var random = Random.Shared;
-        
-        
         var width = absolute.Bounds.Width;
         var height = absolute.Bounds.Height;
 
-/*
-        while (count < 5000)
-        {
-            Dispatcher.UIThread.Post(() =>
-            {
-                var label = new Label
-                {
-                    Content = "lol?",
-                    Foreground = new ImmutableSolidColorBrush(Color.FromRgb((byte)(random.NextSingle() * 255f), (byte)(random.NextSingle() * 255f), (byte)(random.NextSingle() * 255f))),
-                    RenderTransform = new RotateTransform(random.NextDouble() * 360f),
-                    Width = 80,
-                    Height = 40
-                };
-
-                var width = absolute.Bounds.Width;
-                var height = absolute.Bounds.Height;
-                
-                Canvas.SetLeft(label, random.NextDouble() * width);
-                Canvas.SetTop(label, random.NextDouble() * height);
-                if (absolute.Children.Count >= Max)
-                    absolute.Children.RemoveAt(0);
-                absolute.Children.Add(label);
-                count++;
-            });
-
-            Thread.Sleep(1);
-        }
-*/
         while (count < 100000)
         {
             absolute.AddLol(width, height);
@@ -92,7 +57,6 @@ public partial class MainWindow : Window
                 Thread.Sleep(1);
             }
         }
-        
 
         stopwatch.Stop();
         timer.Stop();
