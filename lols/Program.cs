c552bbfcd52d660aefa6c92e47c10838593a2917
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2022-11-04 13:34:44 +0100

    Update Program.cs

--- lols/Program.cs
+++ lols/Program.cs
@@ -6,8 +6,18 @@ namespace lols;
 class Program
 {
     [STAThread]
-    public static void Main(string[] args) => BuildAvaloniaApp()
-        .StartWithClassicDesktopLifetime(args);
+    public static void Main(string[] args)
+    {
+        try
+        {
+            BuildAvaloniaApp().StartWithClassicDesktopLifetime(args);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            throw;
+        }
+    }
 
     public static AppBuilder BuildAvaloniaApp()
         => AppBuilder.Configure<App>()
